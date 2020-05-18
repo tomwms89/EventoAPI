fb47bc17cc99819f55d81dd2855df86fa82aef96
Wellington Macedo da Silva <wellington.macedo89@outlook.com>
2020-05-18 19:20:48 -0300

    start

---
+++ evento/src/main/java/com/qintess/evento/GoEventosApplication.java
@@ -0,0 +1,13 @@
+package com.qintess.evento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoEventosApplication.class, args);
+	}
+
+}
---
+++ evento/src/test/java/com/qintess/evento/GoEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qintess.evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
