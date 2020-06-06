fe4ff319b56f4277902894ce1bc03dae13f1eddc
Wellington Macedo da Silva <re038316@resource.com.br>
2020-06-05 23:17:52 -0300

    update

--- evento/src/main/java/com/qintess/evento/controllers/EventoController.java
+++ evento/src/main/java/com/qintess/evento/controllers/EventoController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,18 +18,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qintess.evento.dados.EventoRepos;
-import com.qintess.evento.dados.LocalShowRepos;
 import com.qintess.evento.entidades.Evento;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EventoController {
 	
 	@Autowired
 	private EventoRepos eventoRepos;
 	
-	@Autowired
-	private LocalShowRepos localShowRepos;
 	
 	@GetMapping("/evento")
 	public List<Evento> getEvento() {
